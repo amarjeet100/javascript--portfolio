9cb1b8bde2334a3fb5ffa115f16e81372ddb0b59
upendra singh <singh.aj200b@gmail.com>
2025-12-04 22:37:57 +0530

    added some comment

--- 1.Variables/script.js
+++ 1.Variables/script.js
@@ -1,5 +1,9 @@
 // some practice on javascript Variables 
  //  You can think of variables as “storage containers” for data in your code.
+ // we declare variables with three keyword let var and const
+ // let, which we can re-assign.
+//const which we can’t re-assign and will throw an error if we try.
+// we will use let and const heavily in my code not var
 
 
 //let age = 25;
@@ -21,19 +25,19 @@ let isStudent = true;
 
 //1. age calculator
 //let birthyear = 2000;
-let birthyear = Number(prompt("Enter your birth year : ")); //  prompt is function it shows a modal window with a text message, an input field for the visitor, and the buttons OK/Cancel.
+//let birthyear = Number(prompt("Enter your birth year : ")); //  prompt is function it shows a modal window with a text message, an input field for the visitor, and the buttons OK/Cancel.
 
 let age = 2025 - birthyear;
-console.log(age);
+//console.log(age);
 
 // 2. Temperature Converter (°C ↔ °F)
 
 //formula --> F = (C × 9/5) + 32
 
-const  celsius = Number(prompt("Enter your Number: "));
+//const  celsius = Number(prompt("Enter your Number: "));
 
 const fahrenheit = (celcuis * 9/5) + 32;
-console.log(fahrenheit + "℉");
+//console.log(fahrenheit + "℉");
 
 
 
